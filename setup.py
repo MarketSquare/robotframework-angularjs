6b03d0212a3e1c4070b67ba8c2352b3d52f7344d
edward-manlove <edward.manlove@dealertrack.com>
2016-01-14 20:05:05 -0500

    Added setup.py for eventual packaging of the library.

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""An AngularJS extension to Robotframework's Selenium2Library
+
+See:
+http://robotframework.org/
+https://github.com/Selenium2Library/robotframework-angularjs
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='robotframework-angularjs',
+    version='0.0.1',
+    description="""An AngularJS extension to Robotframework's Selenium2Library""",
+    long_description=long_description,
+    url='https://github.com/Selenium2Library/robotframework-angularjs',
+    author='Zephraph, Ed Manlove',
+    author_email='zephraph@gmail.com, devPyPlTw@verizon.net',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ],
+    keywords='robotframework testing testautomation angular selenium webdriver',
+    packages=find_packages(exclude=['docs']),
+    install_requires=['robotframework', 'robotframework-selenium2library'],
+)
