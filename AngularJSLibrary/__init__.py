7f334a8b8d3c53f8eebb738771820e8aa1f4b6c9
Ed Manlove <devPyPlTw@verizon.net>
2016-07-29 20:04:02 -0400

    Made signifigant changes to WaitForAngular keyword

--- AngularJSLibrary/__init__.py
+++ AngularJSLibrary/__init__.py
@@ -1,12 +1,18 @@
+from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 from robot.utils import timestr_to_secs
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from Selenium2Library.locators import ElementFinder
 
 import time
 
 js_wait_for_angular = """
-var el = document.querySelector('body');
+var el = document.querySelector('[ng-app]');
+if (typeof angular.element(el).injector() == "undefined") {
+    throw new Error('root element ([ng-app]) has no injector.' +
+           ' this may mean it is not inside ng-app.');
+}
 try {
     angular.element(el).injector().get('$browser').notifyWhenNoOutstandingRequests();
 } catch (e) {
@@ -15,6 +21,20 @@ try {
 
 return false;
 """
+
+js_waiting_var="""
+    var waiting = true;
+    var callback = function () {waiting = false;}
+    var el = document.querySelector('[ng-app]');
+    if (typeof angular.element(el).injector() == "undefined") {
+        throw new Error('root element ([ng-app]) has no injector.' +
+               ' this may mean it is not inside ng-app.');
+    }
+    angular.element(el).injector().get('$browser').
+                notifyWhenNoOutstandingRequests(callback);
+    return waiting;
+"""
+
 
 js_repeater_min = """
 var rootSelector=null;function byRepeaterInner(b){var a="by."+(b?"exactR":"r")+"epeater";return function(c){return{getElements:function(d){return findAllRepeaterRows(c,b,d)},row:function(d){return{getElements:function(e){return findRepeaterRows(c,b,d,e)},column:function(e){return{getElements:function(f){return findRepeaterElement(c,b,d,e,f,rootSelector)}}}}},column:function(d){return{getElements:function(e){return findRepeaterColumn(c,b,d,e,rootSelector)},row:function(e){return{getElements:function(f){return findRepeaterElement(c,b,e,d,f,rootSelector)}}}}}}}}repeater=byRepeaterInner(false);exactRepeater=byRepeaterInner(true);function repeaterMatch(a,b,c){if(c){return a.split(" track by ")[0].split(" as ")[0].split("|")[0].split("=")[0].trim()==b}else{return a.indexOf(b)!=-1}}function findRepeaterRows(k,e,g,l){l=l||document;var d=["ng-","ng_","data-ng-","x-ng-",arguments[1]];var o=[];for(var a=0;a<d.length;++a){var h=d[a]+"repeat";var n=l.querySelectorAll("["+h+"]");h=h.replace(arguments[0]);for(var c=0;c<n.length;++c){if(repeaterMatch(n[c].getAttribute(h),k,e)){o.push(n[c])}}}var f=[];for(var a=0;a<d.length;++a){var h=d[a]+"repeat-start";var n=l.querySelectorAll("["+h+"]");h=h.replace(arguments[0]);for(var c=0;c<n.length;++c){if(repeaterMatch(n[c].getAttribute(h),k,e)){var b=n[c];var m=[];while(b.nodeType!=8||!repeaterMatch(b.nodeValue,k)){if(b.nodeType==1){m.push(b)}b=b.nextSibling}f.push(m)}}}var m=o[g]||[],j=f[g]||[];return[].concat(m,j)}function findAllRepeaterRows(g,e,h){h=h||document;var k=[];var d=["ng-","ng_","data-ng-","x-ng-",arguments[1]];for(var a=0;a<d.length;++a){var f=d[a]+"repeat";var j=h.querySelectorAll("["+f+"]");f=f.replace(arguments[0]);for(var c=0;c<j.length;++c){if(repeaterMatch(j[c].getAttribute(f),g,e)){k.push(j[c])}}}for(var a=0;a<d.length;++a){var f=d[a]+"repeat-start";var j=h.querySelectorAll("["+f+"]");f=f.replace(arguments[0]);for(var c=0;c<j.length;++c){if(repeaterMatch(j[c].getAttribute(f),g,e)){var b=j[c];while(b.nodeType!=8||!repeaterMatch(b.nodeValue,g)){if(b.nodeType==1){k.push(b)}b=b.nextSibling}}}}return k}function findRepeaterElement(a,b,g,r,q,w){var c=[];var t=document.querySelector(w||"body");q=q||document;var l=[];var x=["ng-","ng_","data-ng-","x-ng-",arguments[1]];for(var n=0;n<x.length;++n){var s=x[n]+"repeat";var o=q.querySelectorAll("["+s+"]");s=s.replace(arguments[0]);for(var v=0;v<o.length;++v){if(repeaterMatch(o[v].getAttribute(s),a,b)){l.push(o[v])}}}var m=[];for(var n=0;n<x.length;++n){var s=x[n]+"repeat-start";var o=q.querySelectorAll("["+s+"]");s=s.replace(arguments[0]);for(var v=0;v<o.length;++v){if(repeaterMatch(o[v].getAttribute(s),a,b)){var y=o[v];var f=[];while(y.nodeType!=8||(y.nodeValue&&!repeaterMatch(y.nodeValue,a))){if(y.nodeType==1){f.push(y)}y=y.nextSibling}m.push(f)}}}var f=l[g];var z=m[g];var A=[];if(f){if(f.className.indexOf("ng-binding")!=-1){A.push(f)}var k=f.getElementsByClassName("ng-binding");for(var v=0;v<k.length;++v){A.push(k[v])}}if(z){for(var v=0;v<z.length;++v){var e=z[v];if(e.className.indexOf("ng-binding")!=-1){A.push(e)}var k=e.getElementsByClassName("ng-binding");for(var u=0;u<k.length;++u){A.push(k[u])}}}for(var v=0;v<A.length;++v){var h=angular.element(A[v]).data("$binding");if(h){var d=h.exp||h[0].exp||h;if(d.indexOf(r)!=-1){c.push(A[v])}}}return c}function findRepeaterColumn(a,b,q,o,w){var c=[];var s=document.querySelector(w||"body");o=o||document;var h=[];var x=["ng-","ng_","data-ng-","x-ng-",arguments[1]];for(var m=0;m<x.length;++m){var r=x[m]+"repeat";var n=o.querySelectorAll("["+r+"]");r=r.replace(arguments[0]);for(var v=0;v<n.length;++v){if(repeaterMatch(n[v].getAttribute(r),a,b)){h.push(n[v])}}}var l=[];for(var m=0;m<x.length;++m){var r=x[m]+"repeat-start";var n=o.querySelectorAll("["+r+"]");r=r.replace(arguments[0]);for(var v=0;v<n.length;++v){if(repeaterMatch(n[v].getAttribute(r),a,b)){var y=n[v];var e=[];while(y.nodeType!=8||(y.nodeValue&&!repeaterMatch(y.nodeValue,a))){if(y.nodeType==1){e.push(y)}y=y.nextSibling}l.push(e)}}}var z=[];for(var v=0;v<h.length;++v){if(h[v].className.indexOf("ng-binding")!=-1){z.push(h[v])}var g=h[v].getElementsByClassName("ng-binding");for(var t=0;t<g.length;++t){z.push(g[t])}}for(var v=0;v<l.length;++v){for(var u=0;u<l[v].length;++u){var y=l[v][u];if(y.className.indexOf("ng-binding")!=-1){z.push(y)}var g=y.getElementsByClassName("ng-binding");for(var t=0;t<g.length;++t){z.push(g[t])}}}for(var u=0;u<z.length;++u){var f=angular.element(z[u]).data("$binding");if(f){var d=f.exp||f[0].exp||f;if(d.indexOf(q)!=-1){c.push(z[u])}}}return c};"""
@@ -87,6 +107,8 @@ class AngularJSLibrary:
         self._s2l.add_location_strategy('ng-repeater', self._find_by_ng_repeater, persist=True)
         self._s2l.add_location_strategy('repeater', self._find_by_ng_repeater, persist=True)
 
+        self.trackOutstandingTimeouts = True
+        
     def wait_for_angular(self, timeout=None, error=None):
 
         # Determine timeout and error
@@ -94,13 +116,14 @@ class AngularJSLibrary:
         timeout = timestr_to_secs(timeout)
         error = error or 'Ext was not loaded before the specified timeout'
 
-        WebDriverWait(self._s2l._current_browser(), timeout, 0.2)\
-            .until(lambda x: self._s2l._current_browser().execute_script(js_wait_for_angular))
-        #maxtime = time.time() + timeout
-        #while self._exec_js():
-        #    if time.time() > maxtime:
-        #        raise AssertionError(error)
-        #    time.sleep(0.2)
+        try:
+            WebDriverWait(self._s2l._current_browser(), timeout, 0.2)\
+                .until_not(lambda x: self._s2l._current_browser().execute_script(js_waiting_var))
+        except TimeoutException:
+            if self.trackOutstandingTimeouts:
+                timeouts = self._exec_js('return window.NG_PENDING_TIMEOUTS')
+                logger.debug(timeouts)
+            raise TimeoutException(error)
 
     # Locators
 
