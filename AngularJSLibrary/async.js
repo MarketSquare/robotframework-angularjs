45b025ad18825c8cd3be9ffe4cec0e57273a7912
Ed Manlove <devPyPlTw@verizon.net>
2016-08-21 17:13:45 -0400

    Added test files and updated TESTING.rst.

---
+++ AngularJSLibrary/async.js
@@ -0,0 +1,88 @@
+function AsyncCtrl($scope, $http, $timeout, $location) {
+  $scope.slowHttpStatus = 'not started';
+  $scope.slowFunctionStatus = 'not started';
+  $scope.slowTimeoutStatus = 'not started';
+  $scope.slowAngularTimeoutStatus = 'not started';
+  $scope.slowAngularTimeoutCompleted = false;
+  $scope.slowAngularTimeoutPromiseStatus = 'not started';
+  $scope.slowAngularTimeoutPromiseCompleted = false;
+  $scope.slowHttpPromiseStatus = 'not started';
+  $scope.slowHttpPromiseCompleted = false;
+  $scope.routingChangeStatus = 'not started';
+  $scope.templateUrl = 'fastTemplateUrl';
+
+  $scope.slowHttp = function() {
+    $scope.slowHttpStatus = 'pending...';
+    $http({method: 'GET', url: 'slowcall'}).success(function() {
+      $scope.slowHttpStatus = 'done';
+    });
+  };
+
+  $scope.slowFunction = function() {
+    $scope.slowFunctionStatus = 'pending...';
+    for (var i = 0, t = 0; i < 500000000; ++i) {
+      t++;
+    }
+    $scope.slowFunctionStatus = 'done';
+  };
+
+  $scope.slowTimeout = function() {
+    $scope.slowTimeoutStatus = 'pending...';
+    window.setTimeout(function() {
+      $scope.$apply(function() {
+        $scope.slowTimeoutStatus = 'done';
+      });
+    }, 5000);
+  };
+
+  $scope.slowAngularTimeout = function() {
+    $scope.slowAngularTimeoutStatus = 'pending...';
+    $timeout(function() {
+      $scope.slowAngularTimeoutStatus = 'done';
+      $scope.slowAngularTimeoutCompleted = true;
+    }, 5000);
+  };
+
+  $scope.slowAngularTimeoutHideButton = function() {
+    $scope.slowAngularTimeoutCompleted = false;
+  };
+
+  $scope.slowAngularTimeoutPromise = function() {
+    $scope.slowAngularTimeoutPromiseStatus = 'pending...';
+    $timeout(function() {
+      // intentionally empty
+    }, 5000).then(function() {
+      $scope.slowAngularTimeoutPromiseStatus = 'done';
+      $scope.slowAngularTimeoutPromiseCompleted = true;
+    });
+  };
+
+  $scope.slowAngularTimeoutPromiseHideButton = function() {
+    $scope.slowAngularTimeoutPromiseCompleted = false;
+  };
+
+  $scope.slowHttpPromise = function() {
+    $scope.slowHttpPromiseStatus = 'pending...';
+    $http({method: 'GET', url: 'slowcall'}).success(function() {
+      // intentionally empty
+    }).then(function() {
+      $scope.slowHttpPromiseStatus = 'done';
+      $scope.slowHttpPromiseCompleted = true;
+    });
+  };
+
+  $scope.slowHttpPromiseHideButton = function() {
+    $scope.slowHttpPromiseCompleted = false;
+  };
+
+  $scope.routingChange = function() {
+    $scope.routingChangeStatus = 'pending...';
+    $location.url('slowloader');
+  };
+
+  $scope.changeTemplateUrl = function() {
+    $scope.templateUrl = 'slowTemplateUrl';
+  };
+}
+
+AsyncCtrl.$inject = ['$scope', '$http', '$timeout', '$location'];
