3b671c9c71b5442b340d3758027d9507aa15b66d
Ed Manlove <devPyPlTw@verizon.net>
2019-07-31 21:26:41 -0400

    Bumping version to 0.0.11dev1

--- AngularJSLibrary/__init__.py
+++ AngularJSLibrary/__init__.py
@@ -169,7 +169,7 @@ class ngElementFinder(ElementFinder):
 class AngularJSLibrary:
 
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-    ROBOT_LIBRARY_VERSION = '0.0.10'
+    ROBOT_LIBRARY_VERSION = '0.0.11dev1'
 
     def __init__(self,
                  root_selector=None,
